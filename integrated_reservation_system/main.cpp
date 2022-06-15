1be0d867ab69034a7cc79e2cbb6f9330b69a6569
uwoobeat <desktop3711@g.hongik.ac.kr>
2022-06-15 16:28:57 +0900

    Fix user interface

--- integrated_reservation_system/main.cpp
+++ integrated_reservation_system/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Reservation_Airplane.h"
 #include "Reservation_Restaurant.h"
+#include "Reservation_Library.h"
 
 using namespace std;
 
@@ -8,6 +9,7 @@ int main()
 {
 	Reservation_Airplane* airplaneInterface = new Reservation_Airplane();
 	Reservation_Restaurant* restaurantInterface = new Reservation_Restaurant();
+	Reservation_Library* libraryInterface = new Reservation_Library();
 	int state = 0;
 
 	cout << "통합 예매 프로그램에 오신 것을 환영합니다." << "\n\n";
@@ -16,7 +18,7 @@ int main()
 		switch (state) {
 		case 0:
 			cout << "원하시는 예약 서비스를 선택하세요." << '\n';
-			cout << "비행기 : 1, 식당 : 2, 독서실 : 3, 통계 : 4 , 끝내기 : 5 >> ";
+			cout << "비행기:1, 식당:2, 독서실:3, 끝내기:4 >> ";
 			cin >> state;
 			cout << endl;
 			break;
@@ -29,12 +31,10 @@ int main()
 			state = 0;
 			break;
 		case 3:
+			libraryInterface->run();
 			state = 0;
 			break;
 		case 4:
-			state = 0;
-			break;
-		case 5:
 			cout << "프로그램 종료";
 			exit(1);
 		default:
